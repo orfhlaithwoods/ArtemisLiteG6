90d3f397416aaaddfd0629cedc264e6087ac432e
Órfhlaith Woods <97896391+orfhlaithwoods@users.noreply.github.com>
2022-03-31 16:33:39 +0100

    Update Area.java

--- Area.java
+++ Area.java
@@ -3,94 +3,69 @@ package artemisLite;
 public class Area {
 
 	/*
-	 * 
-	 * 
-	 * 
-	 * instance vars
-	 * 
-	 * 
-	 * 
+	*Instance vars for Area class
+	*
 	 */
 
 	private eSpaceSystem system;
-
 	private String name;
-
 	private int location;
-	
-	
-
 	/**
+	* constants for the Area Class
 	 */
-
 	public static final int MIN_SYSTEM_BOUNDARY = 0;
-
 	public static final int MAX_SYSTEM_BOUNDARY = 12;
-
 	// System default constructor
-
 	public Area() {
-
 	}
 
 	/**
 	 * 
 	 * System argument constructor
-	 *@param system
-	 *@param name
-	 * @param location
+	 *@param system (Enum)
+	 *@param name (String)
+	 * @param location (int)
 	 * 
 	 * 
 	 * 
 	 */
 
 	public Area(eSpaceSystem system, String name, int location) {
-
-		this.system = system;
-
-		this.setName(name);
-
-		this.setLocation(location);
-		
-		
-
-	}
+	this.system = system;
+	this.setName(name);
+	this.setLocation(location);
+		}
 
 	public eSpaceSystem getSystem() {
-
 		return system;
-
 	}
 
 	public void setSystem(eSpaceSystem system) {
-
 		this.system = system;
-
 	}
 
 	public String getName() {
-
 		return name;
-
 	}
 
+	/*
+	*adding logic to fufill requirement
+	*the username cannot be empty
+	*user needs to input a string username to be accepted into the game
+	*otherwise exception will be thrown 
+	*error message will populate
+	*/
+	
 	public void setName(String name) throws IllegalArgumentException {
-
-		if (name != null && !name.isEmpty()) {
-
-			this.name = name;
-
+	if (name != null && !name.isEmpty()) {
+		this.name = name;
 		} else {
-
-			throw new IllegalArgumentException("Please enter a valid name");
-
+		throw new IllegalArgumentException("Please enter a valid name");
 		}
-
 	}
 
 	public int getLocation() {
-
-		return location;
+	return location;
 
 	}
 
@@ -98,11 +73,9 @@ public class Area {
 
 	 * Sets the System location of the board between the min and max System
 	 * boundaries
- 
-	 * @param location
-
-	 * @throws IllegalArgumentException
-
+ 	 * @param location of the board
+	 * @throws IllegalArgumentException 
+ 	 * error message - location out of bounds
 	 */
 
 	public void setLocation(int location) throws IllegalArgumentException {
