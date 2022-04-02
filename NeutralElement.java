8cba1cad9387fbbfb1d4b572f4b81b61801a70eb
Órfhlaith Woods <97896391+orfhlaithwoods@users.noreply.github.com>
2022-04-02 12:15:46 +0100

    Update NeutralElement.java

--- NeutralElement.java
+++ NeutralElement.java
@@ -1,5 +1,14 @@
 package artemisLite;
 
+/**
+*This Class is used to identify the elements which are neutral on the board
+*START and NASA HQ 
+*^ these are our neutral elements
+* they are different as they cannot be purchased or developed by the players
+* when a player passes Start they are credited with BTC
+* when a player lands on or passes NASA HQ nothing happens.
+*/
+
 public class NeutralElement extends Area {
 
 	/**
@@ -11,10 +20,11 @@ public class NeutralElement extends Area {
 
 	/**
 	 * NeutralElements argument constructor
+	 * inherits variables from the Area Super Class
 	 * 
-	 * @param eSpaceSystem
-	 * @param name
-	 * @param location
+	 * @param eSpaceSystem - ENUM class defining system names
+	 * @param name - ELEMENT name
+	 * @param location - Location of element on the board
 	 */
 	public NeutralElement(eSpaceSystem esystem, String name, int location) {
 
