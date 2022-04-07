54a31c64a57dc36ae35ea36c24db2ea319f9e02b
Ryan Bernard Bowman <97897915+RBowman127@users.noreply.github.com>
2022-04-07 18:45:24 +0100

    Add files via upload

--- PlayGame.java
+++ PlayGame.java
@@ -1,9 +1,5 @@
 package artemisLite;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
-
 /**
  * PlayGame class
  * 
@@ -18,21 +14,13 @@ public class PlayGame {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		// instantiates the GameManager class
 		GameAdmin game = new GameAdmin();
-		
+
 		// invokes the playGame method
 		game.startGame();
-		
-		try{
-			
-			PrintStream gameOutput = new PrintStream(new File ("C:\\Users\\Órfhlaith Woods\\eclipse-workspace2\\ArtemisLiteV3 (2).zip_expanded\\ArtemisLiteV3\\gameOutput.txt"));
-			System.setOut(gameOutput);
-			gameOutput.print(game);
-			
-			
-} catch (FileNotFoundException e) { System.out.println(e);}
+
 	} // end of main method
 
 } // end of PlayGame class
